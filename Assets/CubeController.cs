3008e71c1a4276712e089cdf95c653f704cf2696
吉岡茜 <sode_take@icloud.com>
2018-09-30 15:54:30 +0900

    GoForwardUnityChan初めてのコミット

---
+++ Assets/CubeController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+	// キューブの移動速度
+	private float speed = -0.2f;
+
+	// 消滅位置
+	private float deadLine = -10;
+
+	public AudioClip audioClip1;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start(){
+		audioSource = gameObject.GetComponent<AudioSource>();
+		audioSource.clip = audioClip1;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// キューブを移動させる
+		transform.Translate (this.speed, 0, 0);
+
+		// 画面外に出たら破棄する
+		if (transform.position.x < this.deadLine){
+			Destroy (gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Cube") {
+			audioSource.Play ();
+		}else if(coll.gameObject.tag=="Ground"){
+			audioSource.Play();
+		}
+	}
+}
